4c3dcff90f2e8b6b8d692a2df5157bba392b6927
yash240408 <yash.mistry.g43@gmail.com>
2023-06-04 18:14:38 +0530

    Updated index.py

--- index.py
+++ index.py
@@ -13,7 +13,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-
+# Cache Control
 @app.after_request
 def after_request(response):
     """Ensure responses aren't cached"""
